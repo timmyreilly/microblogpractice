a2a1fb357a9f73e4987dce485c05338f284f7300
Tim Reilly <timreilly@live.com>
2015-09-01 17:15:55 -0700

    templates

--- microblog/urls.py
+++ microblog/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
