689182cb934f94afbd100364b8ba9ed14f6e026b
Tim Reilly <timreilly@live.com>
2015-09-01 17:00:43 -0700

    5pm

--- microblog/settings/local.py
+++ microblog/settings/local.py
@@ -1,5 +1,14 @@
 #local.py
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
