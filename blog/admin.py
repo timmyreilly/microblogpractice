26c6835ace97a07f7ea0e1396bf9656dbbbf9234
Tim Reilly <timreilly@live.com>
2015-09-01 19:21:14 -0700

    admin and pytz

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	date_hierarchy = "created_at"
+
+admin.site.register(Post, PostAdmin)
+
